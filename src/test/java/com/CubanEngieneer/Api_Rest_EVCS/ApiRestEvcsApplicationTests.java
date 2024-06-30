1654ebdda6d40bc17feb98b78d6eb9a46bbc1e9e
Franky Rodriguez <frankyan2001@gmail.com>
2024-06-30 02:43:28 -0400

    Final Commit

--- src/test/java/com/CubanEngieneer/Api_Rest_EVCS/ApiRestEvcsApplicationTests.java
+++ src/test/java/com/CubanEngieneer/Api_Rest_EVCS/ApiRestEvcsApplicationTests.java
@@ -2,8 +2,10 @@ package com.CubanEngieneer.Api_Rest_EVCS;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ApiRestEvcsApplicationTests {
 
 	@Test
